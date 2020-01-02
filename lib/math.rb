095ec633cce77f0c57034681f60434044155c34b
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 00:42:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,9 +10,9 @@ def division(num1, num2)
   num1/num2
 end
 
-# def multiplication(num1, num2)
-#   num1*num2
-# end
+def multiplication(num1, num2)
+  num1*num2
+end
 
 # def modulo(num1, num2)
 #   num1//num2
