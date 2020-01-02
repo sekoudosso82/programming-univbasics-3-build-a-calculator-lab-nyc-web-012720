d1bd3269e9aed92cc87334451127547d48a6fe3c
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 00:41:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,18 +6,18 @@ end
 #   num1 - num2
 # end
 
-def division(num1, num2)
-  num1/num2
-end
+# def division(num1, num2)
+#   num1/num2
+# end
 
-def multiplication(num1, num2)
-  num1*num2
-end
+# def multiplication(num1, num2)
+#   num1*num2
+# end
 
-def modulo(num1, num2)
-  num1//num2
-end
+# def modulo(num1, num2)
+#   num1//num2
+# end
 
-def square_root(num)
-  Math.sqrt(num)
-end
+# def square_root(num)
+#   Math.sqrt(num)
+# end
